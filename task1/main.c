06479884d3da861fa6762d01100ab2b37a6f2ac1
lizejia2361 <1719083859@qq.com>
2022-10-26 20:13:10 +0800

    add GEEK

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main(){
+	double y=0;
+	int d=1;
+	double a=1,b,x;
+	printf("a=");
+	printf("x     y     \n");
+	b=0.9990005*a;
+	y=b+1.0;
+	for(x=0.001;y-b>0.000001;d+=1)
+	{
+		x=0.001*d;
+		y=b;
+		printf("%f   %f\n",x,y);
+		a=y;
+		b=0.9990005*a;
+		
+	}
 	return 0;
 }
+ 
 
+
+ 
